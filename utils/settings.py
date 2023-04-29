99cc4c7e86d328fe3dde3221ad526b8afa5ec30d
tobyrcod <61169763+tobyrcod@users.noreply.github.com>
2023-04-29 21:38:30 +0100

    Project Setup

---
+++ utils/settings.py
@@ -0,0 +1,12 @@
+import pygame
+pygame.init()
+
+GRID_SIZE = pygame.Vector2(100, 100)
+CELL_SIZE = 5
+WINDOW_SIZE = GRID_SIZE * CELL_SIZE
+
+FPS = 100
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
